543f35788ad36558fd6250e00b0cd9db003acf53
Sorcerer <Sorcerer@192.168.1.102>
2020-10-01 20:23:31 +0300

    Добавлен вызов метода "validateExistingProjectNames" в метод "earlyStartup" класса Startup.

--- project.name.validator/src/project/name/validator/main/Startup.java
+++ project.name.validator/src/project/name/validator/main/Startup.java
@@ -7,8 +7,10 @@ import project.name.validator.ChangedNameValidator;
 public class Startup implements IStartup
 {
 	@Override
-	public void earlyStartup()
+	public void earlyStartup ()
 	{
-		new ChangedNameValidator().addChangedNameListeners();
+		ChangedNameValidator validator = new ChangedNameValidator();
+		validator.validateExistingProjectNames();
+		validator.addChangedNameListeners();
 	}
 }
