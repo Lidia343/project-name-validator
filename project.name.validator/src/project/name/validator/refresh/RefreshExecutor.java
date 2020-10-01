d29c72a2a723cb80fb7e856db3c8b493b57556ee
Sorcerer <Sorcerer@192.168.1.102>
2020-10-01 23:23:32 +0300

    Добавлен класс для обновления проектов.

---
+++ project.name.validator/src/project/name/validator/refresh/RefreshExecutor.java
@@ -0,0 +1,19 @@
+package project.name.validator.refresh;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.IHandlerService;
+
+public class RefreshExecutor
+{
+	public static final String COMMAND_ID = "org.eclipse.ui.file.refresh";
+	
+	public void refresh () throws ExecutionException, NotDefinedException, NotEnabledException, NotHandledException
+	{
+		 IHandlerService handlerService = PlatformUI.getWorkbench().getService(IHandlerService.class);
+		 handlerService.executeCommand(COMMAND_ID, null);
+	}
+}
