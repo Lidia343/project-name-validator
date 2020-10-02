28fc8ff0acb314efa6e58e5bc22918974cc0558c
Sorcerer <Sorcerer@192.168.1.102>
2020-10-02 13:48:26 +0300

    В классе ProblemNameMarkerManager изменено имя и класс поля IProject m_project (теперь - IResource m_resource) для большего абстрагирования. Добавлены комментарии.

--- project.name.validator/src/project/name/validator/marker/ProblemNameMarkerManager.java
+++ project.name.validator/src/project/name/validator/marker/ProblemNameMarkerManager.java
@@ -3,38 +3,60 @@ package project.name.validator.marker;
 import java.util.Objects;
 
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 
+/**
+ * Класс для управления процессами создания и удаления
+ * маркера.
+ */
 public class ProblemNameMarkerManager
 {
 	public static final String MARKER_ATTRIBUTE_PROBLEM_NAME = "MARKER_ATTRIBUTE_PROBLEM_NAME";
 	
 	public static final String MARKER_ATTRIBUTE_VALUE_PROBLEM_NAME = "problemName";
 	
-	public static final String MARKER_ATTRIBUTE_VALUE_MESSAGE = "Имя проекта должно совпадать с именем папки, " +
-																"в которой он находится.";
+	public static final String MARKER_ATTRIBUTE_VALUE_MESSAGE = "Имя проекта должно совпадать с именем его папки " +
+																"в файловой системе.";
 	
-	private IProject m_project;
+	private IResource m_resource;
 	
-	public ProblemNameMarkerManager (IProject a_project)
+	/**
+	 * Конструктор класса ProblemNameMarkerManager.
+	 * @param a_resource
+	 * 		  Ресурс, к которому будет привязан маркер.
+	 * 		  NotNull
+	 */
+	public ProblemNameMarkerManager (IResource a_resource)
 	{
-		m_project = Objects.requireNonNull(a_project);
+		m_resource = Objects.requireNonNull(a_resource);
 	}
 	
+	/**
+	 * Создаёт проблемный маркер, указывающий на неверное
+	 * имя ресурса, если данный маркер ещё не был создан.
+	 * @return true - если маркер был создан, false -
+	 * иначе 
+	 * @throws CoreException
+	 */
 	public boolean createMarker () throws CoreException
 	{
 		if (markerExists()) return false;
-		IMarker marker = m_project.createMarker(IMarker.PROBLEM);
+		IMarker marker = m_resource.createMarker(IMarker.PROBLEM);
 		marker.setAttribute(MARKER_ATTRIBUTE_PROBLEM_NAME, MARKER_ATTRIBUTE_VALUE_PROBLEM_NAME);
-		marker.setAttribute(IMarker.LOCATION, m_project.getLocation().toString());
+		marker.setAttribute(IMarker.LOCATION, m_resource.getLocation().toString());
 		marker.setAttribute(IMarker.MESSAGE, MARKER_ATTRIBUTE_VALUE_MESSAGE);
 		marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
 		marker.setAttribute(IMarker.USER_EDITABLE, false);
 		return true;
 	}
 	
+	/**
+	 * Удаляет маркер, если он существует.
+	 * @return true - если маркер был удалён, false -
+	 * иначе
+	 * @throws CoreException
+	 */
 	public boolean deleteMarker () throws CoreException
 	{
 		IMarker marker = findMarker();
@@ -46,15 +68,26 @@ public class ProblemNameMarkerManager
 		return false;
 	}
 	
+	/**
+	 * @return true - если маркер существует, false -
+	 * иначе
+	 * @throws CoreException
+	 */
 	public boolean markerExists () throws CoreException
 	{
 		if (findMarker() != null) return true;
 		return false;
 	}
 	
+	/**
+	 * @return первый найденный на ресурсе проблемный
+	 * маркер, указывающий на неверное имя ресурса.
+	 * Может быть null.
+	 * @throws CoreException
+	 */
 	public IMarker findMarker () throws CoreException
 	{
-		IMarker[] markers = m_project.findMarkers(IMarker.PROBLEM, false, IResource.DEPTH_INFINITE);
+		IMarker[] markers = m_resource.findMarkers(IMarker.PROBLEM, false, IResource.DEPTH_INFINITE);
 		for (IMarker marker : markers)
 		{
 			if (marker.getAttribute(MARKER_ATTRIBUTE_PROBLEM_NAME, null) != null)
@@ -65,8 +98,11 @@ public class ProblemNameMarkerManager
 		return null;
 	}
 	
-	public IProject getProject ()
+	/**
+	 * @return ресурс, к которому привязан маркер
+	 */
+	public IResource getResource ()
 	{
-		return m_project;
+		return m_resource;
 	}
 }
